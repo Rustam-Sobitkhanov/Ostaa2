ebd1b24751cde5121586b5fdc83087f96153f65a
Rustam Sobitkhanov <sobitxanovr@gmail.com>
2023-04-04 21:08:33 -0700

    ostaa part 2

---
+++ public_html/script.js
@@ -0,0 +1,157 @@
+var username = ""
+function searchListings() {
+    const searchItem = document.getElementById('searchItem').value;
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            const items = JSON.parse(this.responseText);
+            const searchResultDiv = document.getElementById('searchResult');
+            searchResultDiv.innerHTML = '';
+            items.forEach(item => {
+                const itemDiv = document.createElement('div');
+                var status = item.stat;
+                if (status.toLowerCase() === 'sale') {
+                    itemDiv.innerHTML = `
+                    <h3>${item.title}</h3>
+                    <p>${item.description}</p>
+                    <p>${item.image}</p>
+                    <p>${item.price}</p>
+                    <button type="button" name="buy">Buy</button>
+                    `;
+                    const buyButton = itemDiv.querySelector('button[name="buy"]');
+                    buyButton.addEventListener('click', function() {
+                        this.innerText = 'SOLD';
+                        this.disable();
+                        purchase(item); // call your buyItem function here and pass in the item object
+                    });
+                } else {
+                    itemDiv.innerHTML = `
+                    <h3>${item.title}</h3>
+                    <p>${item.description}</p>
+                    <p>${item.image}</p>
+                    <p>${item.price}</p>
+                    <p class="sold">SOLD</p>
+                    `;
+                }
+                itemDiv.classList.add("search-item");
+                searchResultDiv.appendChild(itemDiv);
+            });
+        }
+    };
+    xhr.open('POST', '/search', true);
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    xhr.send(`searchItem=${searchItem}`);
+}
+
+
+function getListings() {
+    var usrName = username;
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            const items = JSON.parse(this.responseText);
+            const searchResultDiv = document.getElementById('searchResult');
+            searchResultDiv.innerHTML = '';
+            items.forEach(item => {
+                const itemDiv = document.createElement('div');
+                itemDiv.innerHTML = `
+                <h3>${item.title}</h3>
+                <p>${item.description}</p>
+                <img src="${item.image}" alt="${item.title}">
+                <p>${item.price}</p>
+                <p>${item.stat}</p>
+        `;
+                searchResultDiv.appendChild(itemDiv);
+            });
+        }
+    };
+    xhr.open('POST', '/listings', true);
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    xhr.send(`usrName=${usrName}`);
+}
+
+
+function addItem() {
+    updateUserOnly();
+    var usrName = username;
+
+    // Get the values of all the form inputs
+    var title = document.getElementById("title-input").value;
+    var description = document.getElementById("description-input").value;
+    var price = document.getElementById("price-input").value;
+    var image = document.getElementById("image-input").value;
+    var stat = document.getElementById("status-input").value;
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', '/addItem', true);
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    xhr.send(`usrName=${usrName}&title=${title}&desc=${description}&price=${price}&img=${image}&stat=${stat}`);
+    if (document.getElementById("add-item-form")) {
+        document.getElementById("add-item-form").reset();
+    }
+}
+
+function viewPurchase() {
+    updateUserOnly();
+    var usrName = username;
+
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            const items = JSON.parse(this.responseText);
+            const searchResultDiv = document.getElementById('searchResult');
+            searchResultDiv.innerHTML = '';
+            items.forEach(item => {
+                const itemDiv = document.createElement('div');
+                itemDiv.innerHTML = `
+                <h3>${item.title}</h3>
+                <p>${item.description}</p>
+                <img src="${item.image}" alt="${item.title}">
+                <p>${item.price}</p>
+                `;
+                searchResultDiv.appendChild(itemDiv);
+            });
+        }
+    };
+
+    xhr.open('POST', '/viewPurchases', true);
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    xhr.send(`usrName=${usrName}`);
+}
+
+
+function redirectToListing() {
+    window.location.href = '/listing.html';
+}
+
+function updateUser() {
+    var cookieValue = getCook('username');
+    username = cookieValue;
+    if (cookieValue) {
+        document.getElementById("welcome-message").innerText = "Welcome " + cookieValue;
+    }
+}
+
+function updateUserOnly() {
+    var cookieVal = getCook('username');
+    username = cookieVal;
+}
+
+function getCook(cookiename) {
+    // Get name followed by anything except a semicolon
+    var cookiestring=RegExp(cookiename+"=[^;]+").exec(document.cookie);
+    // Return everything after the equal sign, or an empty string if the cookie name not found
+    return decodeURIComponent(!!cookiestring ? cookiestring.toString().replace(/^[^=]+./,"") : "");
+}
+
+function purchase(item) {
+    updateUserOnly();
+    var usrName = username;
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', '/purchaseItem', true);
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    xhr.send(`usrName=${usrName}&title=${item.title}&desc=${item.description}&price=${item.price}&img=${item.image}&stat=${item.stat}`);
+    document.getElementById("add-item-form").reset();
+}
+
---
+++ server.js
@@ -0,0 +1,223 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const axios = require('axios');
+
+var curUser = "";
+
+// Use body-parser middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static('public_html'));
+
+app.use(cookieParser());
+app.get('/setCookie/:username', function (req, res, next) {
+    var user = req.params.username;
+    res.cookie('username', user);
+    res.redirect('/home.html');
+    next();
+});
+
+
+mongoose.connect('mongodb+srv://sobitxanovr:2ik19goHn21Ej0RL@ostaa.jf7rett.mongodb.net/?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log('MongoDB connected...'))
+    .catch((err) => console.log(err));
+
+const itemSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    image: String,
+    price: Number,
+    stat: String
+});
+
+const Item = mongoose.model('Item', itemSchema);
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    listings: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Item' }],
+    purchases: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Item' }]
+});
+
+const User = mongoose.model('User', userSchema);
+
+app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/public_html/index.html`);
+});
+
+
+// Handle login request
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (user && user.password === password) {
+        curUser = username;
+        res.redirect('/setCookie/' + username);
+    } else {
+        res.send('Invalid username or password');
+    }
+});
+
+// Handle registration request
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (user) {
+        res.send('User already exists');
+    } else {
+        curUser = username;
+        const newUser = new User({ username, password });
+        await newUser.save();
+        res.redirect('/setCookie/' + username);
+    }
+});
+
+app.post('/search', async (req, res) => {
+    const { searchItem } = req.body;
+    const regex = new RegExp(searchItem, 'i');
+    const items = await Item.find({ description: regex });
+    res.json(items);
+});
+
+
+app.post('/listings', async (req, res) => {
+    const { usrName } = req.body;
+    try {
+        const user = await User.findOne({username: usrName});
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+        const listings = user.listings;
+
+        Item.find({ _id: { $in: listings } }).exec()
+            .then(docs => {
+                res.json(docs);
+            })
+            .catch(err => {
+                // Handle error
+            });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: 'Failed to retrieve user listings' });
+    }
+});
+
+
+app.post('/viewPurchases', async (req, res) => {
+    const { usrName } = req.body;
+    try {
+        const user = await User.findOne({username: usrName});
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+        const purchases = user.purchases;
+
+        Item.find({ _id: { $in: purchases } }).exec()
+            .then(docs => {
+                res.json(docs);
+            })
+            .catch(err => {
+                // Handle error
+            });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: 'Failed to retrieve user purchases' });
+    }
+});
+
+
+
+app.post('/addItem', async (req, res) => {
+    const { usrName, title, desc, price, img, stat } = req.body;
+    User.findOne({username: usrName})
+        .then(user => {
+            if (!user) {
+                return res.status(500).json({ error: 'User: not found' });
+            }
+            const item = new Item({ title: title, description: desc, image: img, price: price, stat: stat });
+            return item.save().then(item => {
+                user.listings.push(item._id);
+                user.save();
+                return;
+            }).then(() => {
+                console.log(`New item added to ${user.username}'s listings: ${item.title}`);
+                return res.status(200).json({ message: 'Item added successfully' });
+            });
+        })
+        .catch(err => {
+            console.error(err);
+            return res.status(500).json({ error: 'Failed to add item to user' });
+        });
+});
+
+app.post('/purchaseItem', async (req, res) => {
+    const { usrName, title, price, img } = req.body;
+    User.findOne({ username: usrName })
+        .then(user => {
+            if (!user) {
+                return res.status(500).json({ error: 'User not found' });
+            }
+            Item.findOne({ title: title, price: price, image: img })
+                .then(item => {
+                    if (!item) {
+                        return res.status(500).json({ error: 'Item not found' });
+                    }
+                    item.stat = 'sold';
+                    item.save();
+
+                    user.purchases.push(item._id);
+                    user.save();
+                    console.log(`Item ${item.title} added to ${user.username}'s purchases`);
+                    return res.status(200).json({ message: 'Item purchased successfully' });
+                })
+                .catch(err => {
+                    console.error(err);
+                    return res.status(500).json({ error: 'Failed to add item to user purchases' });
+                });
+        })
+        .catch(err => {
+            console.error(err);
+            return res.status(500).json({ error: 'Failed to find user' });
+        });
+});
+
+
+app.get('/image/:imageprompt', async (req, res) => {
+
+    try {
+        const imageprompt = req.params.imageprompt;
+        console.log(imageprompt)
+        const response = await axios({
+            method: 'post',
+            url: 'https://api.openai.com/v1/images/generations',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${}`
+            },
+            data: {
+                model: 'image-alpha-001',
+                prompt: `Generate an image of ${imageprompt}`,
+                n: 3,
+                size: '256x256',
+                response_format: 'url'
+            }
+        });
+
+        const imageUrl = response.data.data[0].url;
+        res.send(`<img src="${imageUrl}" alt="${imageprompt}">`);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error generating image');
+    }
+});
+
+
+app.listen(3000, () => {
+    console.log('Server is listening on port 3000...');
+});
